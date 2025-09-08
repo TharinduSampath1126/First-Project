399ff53a056b509603091aa3f9efc00f78ecd49a
TharinduSampath <tharindu.s@botcalm.com>
2025-09-09 01:00:25 +0530

    commit 7

--- src/sections/Section4.jsx
+++ src/sections/Section4.jsx
@@ -3,12 +3,12 @@ import React from 'react'
 export default function Section4() {
   return (
 
-    <section className="px-4 py-8 md:py-12">
-      <div className="space-y-6 text-center max-w-2xl mx-auto">
+    <section className="px-4 py-10 md:py-12">
+      <div className="space-y-6 text-center mx-auto">
         <p className="font-poppins font-semibold text-2xl md:text-3xl lg:text-4xl leading-snug  tracking-normal">
           Lorem Ipsum is simply dummy text of the printing.
         </p>
-        <p className="font-poppins font-normal text-base md:text-lg lg:text-xl leading-relaxed md:leading-[175%] tracking-normal text-[#868686]">
+        <p className="font-poppins font-normal text-base md:text-lg lg:text-xl max-w-3xl justify-self-center leading-relaxed md:leading-[175%] tracking-normal text-[#868686]">
           Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's
         </p>
       </div>
